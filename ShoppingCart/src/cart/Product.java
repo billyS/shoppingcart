52353b965aee4f0bffd395d80565f609b16e4aeb
user10 <user10@DESKTOP-JED5T71.Home>
2017-11-20 13:05:27 +0000

    Creating Product Class

--- ShoppingCart/src/cart/Product.java
+++ ShoppingCart/src/cart/Product.java
@@ -2,4 +2,20 @@ package cart;
 
 public class Product {
 
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	private double price;
+	private String productName;
+	
+	
 }
